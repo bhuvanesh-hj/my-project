926c041bf7ce2f70e281ba9b38c25eb16988fe55
bhuvanesh-hj <144657401+bhuvanesh-hj@users.noreply.github.com>
2023-10-02 14:08:09 +0530

    Add files via upload

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./Button.css";
+import classes from './Button.module.css';
 
 const Button = (props) => {
   return (
     <button
-      type={props.type}
-      className={`button ${!props.valid ? "invalid" : ""}`}
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
