8f2cacee7f510f734d98bf993483d8cc2591f45f
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-04 22:10:42 +0530

    added the method overlay by using the portal

--- Creating a restaurant app/src/App.js
+++ Creating a restaurant app/src/App.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals.js";
-
+import Cart from "./components/Cart/Cart.js";
 import Header from "./components/Layout/Header.js";
 function App() {
   return (
     <Fragment>
+      <Cart/>
      <Header/>
      <main>
     <Meals/>
---
+++ Creating a restaurant app/src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+const Cart = () => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: "c1", nmae: "milk", price: 40, amount: 1 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>22</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
