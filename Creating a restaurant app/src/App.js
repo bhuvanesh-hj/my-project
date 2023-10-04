a08f64bbb60f5bad58a7f363ff998f76c8bc595d
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-04 19:48:51 +0530

    added the meal list amd rendered on the page

--- Creating a restaurant app/src/App.js
+++ Creating a restaurant app/src/App.js
@@ -1,9 +1,14 @@
 import { Fragment } from "react";
+import Meals from "./components/Meals/Meals.js";
+
 import Header from "./components/Layout/Header.js";
 function App() {
   return (
     <Fragment>
      <Header/>
+     <main>
+    <Meals/>
+     </main>
     </Fragment>
   );
 }
