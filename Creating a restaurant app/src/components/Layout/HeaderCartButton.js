76c3dc8fed81c3c94e6c00b390a392d8fd1747e8
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-05 16:01:17 +0530

    added the animation to the cart button

--- Creating a restaurant app/src/components/Layout/HeaderCartButton.js
+++ Creating a restaurant app/src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,33 @@
 import classes from "./HeaderCartButton.module.css";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import cartContext from "../../store/cartContext";
 
 const HeaderCartButton = (props) => {
+  const [cartBtnVaild, setCartBtnVaild] = useState(false);
   const ctx = useContext(cartContext);
-
-  const numberOfItems = ctx.items.reduce((currentNumber, item) => {
+  const { items } = ctx;
+  const numberOfItems = items.reduce((currentNumber, item) => {
     return currentNumber + +item.amount;
   }, 0);
-  // let quantity = 0;
-  // ctx.items.forEach((item) => {
-  //   quantity += +item.quantity;
-  // });
+
+  const btnAnimation = `${classes.button} ${cartBtnVaild ? classes.bump : " "}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setCartBtnVaild(true);
+    const timer = setTimeout(() => {
+      setCartBtnVaild(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={() => props.shows(true)}>
+    <button className={btnAnimation} onClick={() => props.shows(true)}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
