9660b32b3a6b370324106c55026944cb2026fbc9
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-09 18:25:25 +0530

    setTimer is added

--- Making-api-request/src/App.js
+++ Making-api-request/src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [errorHandler, setErrorHandler] = useState(null);
+  const [retry, setRetry] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsloading(true);
+    setErrorHandler(null);
     try {
       const MovieData = await fetch("https://swapi.dev/api/films");
+
+      if (!MovieData.ok) {
+        setRetry(true);
+        throw new Error("Something went wrong....Retrying");
+      }
 
       const MoviesList = await MovieData.json();
 
@@ -21,10 +31,27 @@ function App() {
         };
       });
       setMovieList(transFormedData);
+      setRetry(false);
     } catch (error) {
-      console.error(error);
+      setErrorHandler(error.message);
     }
+    setIsloading(false);
   }
+
+  useEffect(() => {
+    let timer;
+    if (retry) {
+      timer = setInterval(fetchMovieHandler, 5000);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [retry]);
+
+  const cancelRetry = () => {
+    setRetry(false);
+    setErrorHandler("Canceled");
+  };
 
   return (
     <React.Fragment>
@@ -32,7 +59,15 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movieList} />
+        {!isLoading && <MoviesList movies={movieList} />}
+        {!isLoading && errorHandler && (
+          <p>
+            {errorHandler}
+            {"  "}
+            <button onClick={cancelRetry}>Cancle</button>
+          </p>
+        )}
+        {isLoading && <i>Loading....</i>}
       </section>
     </React.Fragment>
   );
