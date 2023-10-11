bea422f017eec717af105669e64bb6c89b8e5353
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-11 20:32:30 +0530

    first deployed project

---
+++ Deployment-Demo/src/App.js
@@ -0,0 +1,57 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home";
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+
+// the import function always returns the promise function we have to handle them
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "posts",
+        children: [
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                {" "}
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                {" "}
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
