99ef777e5061f24d2be9d098b17dd95c87192e6b
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-18 10:15:40 +0530

    added 5 test case for signup form

---
+++ email-client-box/src/components/Authentication/Athentication.test.js
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import Authentication from "./Authentication";
+
+describe("Authentication part test", () => {
+  test("Checking the sigup", () => {
+    render(<Authentication />);
+    expect(screen.getByText("Signup")).toBeInTheDocument();
+  });
+  test("Checking the email input", () => {
+    render(<Authentication />);
+    expect(
+      screen.getByPlaceholderText("Enter your email", { exact: false })
+    ).toBeInTheDocument();
+  });
+  test("Checking the Password", () => {
+    render(<Authentication />);
+    expect(
+      screen.getByPlaceholderText("Enter your password")
+    ).toBeInTheDocument();
+  });
+  test("Checking the confirm password", () => {
+    render(<Authentication />);
+    expect(
+      screen.getByPlaceholderText("Confirm your password")
+    ).toBeInTheDocument();
+  });
+  test("checking the sigup button", () => {
+    render(<Authentication />);
+    expect(screen.getByText("Sign up")).toBeInTheDocument();
+  });
+  test("Checking the login setup", () => {
+    render(<Authentication />);
+    expect(screen.getByText("Already have an account?")).toBeInTheDocument();
+  });
+});
