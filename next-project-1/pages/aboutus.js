79c94755b83c7859f528b070ae2ae4e67d3d1142
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-20 14:45:50 +0530

    created the about us page

---
+++ next-project-1/pages/aboutus.js
@@ -0,0 +1,5 @@
+function aboutUsPage() {
+    return <h1>This is AboutUs page</h1>;
+  }
+  
+  export default aboutUsPage;
---
+++ next-project-1/pages/news.js
@@ -0,0 +1,5 @@
+function newsPage() {
+    return <h1>This is News page</h1>;
+  }
+  
+  export default newsPage;
