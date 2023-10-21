10e015df70e72b08cec64859cb5e2f95a727d7fe
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2023-10-21 13:51:22 +0530

    added the getStaticProps  and getStaticpPaths

--- 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -4,11 +4,54 @@ const meetupId = () => {
   return (
     <MeetupDetail
       image="https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=600"
+      id="m1"
       title="A first meetup"
       description="This ia a first meetup"
       address="Some road 23, Some city"
     />
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=600",
+        id: meetupId,
+        title: "A first meetup",
+        description: "This ia a first meetup",
+        address: "Some road 23, Some city",
+      },
+    },
+  };
+}
 
 export default meetupId;
--- 06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,30 +1,39 @@
-import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
 
-const HomePage = () => {
-  const DUMMY_LIST = [
-    {
-      id: "m1",
-      adress: "Some street 12,Some great city",
-      description: "This is the first meetup",
-      title: "A First Meetup",
-      image:
-        "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      id: "m2",
-      adress: "Some street 11,Some great city",
-      description: "This is the Second meetup",
-      title: "A Second Meetup",
-      image:
-        "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=600",
-    }
-  ];
+const DUMMY_LIST = [
+  {
+    id: "m1",
+    adress: "Some street 12,Some great city",
+    description: "This is the first meetup",
+    title: "A First Meetup",
+    image:
+      "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: "m2",
+    adress: "Some street 11,Some great city",
+    description: "This is the Second meetup",
+    title: "A Second Meetup",
+    image:
+      "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+const HomePage = (props) => {
   return (
     <>
-      <MeetupList meetups={DUMMY_LIST} onclick={"m1"} />
+      <MeetupList meetups={props.meetups} />
     </>
   );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_LIST,
+    },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
